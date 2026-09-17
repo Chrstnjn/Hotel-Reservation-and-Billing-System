95dcfcacbed1af89905bdd96a083420d1e1fa715
Chrstnjn <lapuzchristinejane@gmail.com>
2026-09-17 22:10:33 +0800

    Organizing the four required project structure folders

---
+++ HotelReservation.Businesslogic/Controller/UserController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation.Businesslogic.Controller
+{
+    internal class UserController
+    {
+    }
+}
---
+++ HotelReservation.Businesslogic/Repository/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation.Businesslogic.Repository
+{
+    internal class UserRepository
+    {
+    }
+}
