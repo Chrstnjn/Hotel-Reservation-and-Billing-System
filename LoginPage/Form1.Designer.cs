212575877a78413ce367e6e8146fb4f0e203388f
Chrstnjn <lapuzchristinejane@gmail.com>
2026-09-17 14:16:05 +0800

    Hotel Reservation Starting System

---
+++ LoginPage/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LoginPage
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(218)))), ((int)(((byte)(224)))));
+            this.panel1.Location = new System.Drawing.Point(296, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 418);
+            this.panel1.TabIndex = 0;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(69)))), ((int)(((byte)(115)))));
+            this.ClientSize = new System.Drawing.Size(891, 518);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmLogin";
+            this.Text = "Login Page";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
